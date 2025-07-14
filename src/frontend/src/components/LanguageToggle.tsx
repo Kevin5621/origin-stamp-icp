26f9506808391979176a7d2730d80dc80d5a8da5
Kevin5621 <yohanes123kevin123@gmail.com>
2025-07-14 11:01:47 +0700

    stle: flag update

--- src/frontend/src/components/LanguageToggle.tsx
+++ src/frontend/src/components/LanguageToggle.tsx
@@ -57,27 +57,15 @@ export function LanguageToggle({ className = "" }: LanguageToggleProps) {
           </clipPath>
           <g clip-path="url(#a)">
             <path d="M0 0v30h60V0z" fill="#012169" />
-            <path
-              d="M0 0l60 30m0-30L0 30"
-              stroke="#fff"
-              stroke-width="6"
-            />
+            <path d="M0 0l60 30m0-30L0 30" stroke="#fff" stroke-width="6" />
             <path
               d="M0 0l60 30m0-30L0 30"
               clip-path="url(#b)"
               stroke="#C8102E"
               stroke-width="4"
             />
-            <path
-              d="M30 0v30M0 15h60"
-              stroke="#fff"
-              stroke-width="10"
-            />
-            <path
-              d="M30 0v30M0 15h60"
-              stroke="#C8102E"
-              stroke-width="6"
-            />
+            <path d="M30 0v30M0 15h60" stroke="#fff" stroke-width="10" />
+            <path d="M30 0v30M0 15h60" stroke="#C8102E" stroke-width="6" />
           </g>
         </svg>
       )}
