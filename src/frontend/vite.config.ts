7f87ae96c6a273247719a75012c7be14081d35a5
Budi Komarudin <90817826+redhawkeye@users.noreply.github.com>
2025-08-09 10:52:29 +0000

    refactor: Remove unused back navigation handler in SettingsPage; streamline code for better readability

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -13,6 +13,16 @@ export default defineConfig({
   build: {
     outDir: "dist/",
     emptyOutDir: true,
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
+    },
   },
   optimizeDeps: {
     esbuildOptions: {
