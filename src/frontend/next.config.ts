2e53fed0b4e09e78e6efa2dc64956826537a70dd
Kevin5621 <yohanes123kevin123@gmail.com>
2025-09-02 18:46:07 +0700

    chore: Update outputFileTracingRoot in Next.js configuration for correct path

--- src/frontend/next.config.ts
+++ src/frontend/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  outputFileTracingRoot: "/home/kevin/Documents/IC-Vibe-Coding-Template-Rust",
+  outputFileTracingRoot: "/home/kevin/Documents/origin-stamp-icp",
 };
 
 export default nextConfig;
